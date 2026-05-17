b06628d559444d9731f8c81b169f2cee926704dc
paerma <555rostik556@ukr.net>
2026-05-17 15:56:11 +0200

    changed exception to IllegalArgumentException

--- src/main/java/com/example/blackjack/Card.java
+++ src/main/java/com/example/blackjack/Card.java
@@ -11,9 +11,9 @@ public class Card {
     private final int value;
     private final suits cardSuit;
 
-    public Card(int value, suits cardSuit) throws Exception {
+    public Card(int value, suits cardSuit) {
         if(value < 1 || value > 11){
-            throw new Exception("неправильне значення карти!");
+            throw new IllegalArgumentException("Неправильне значення карти: " + value + ". Має бути від 1 до 11.");
         }
         this.value = value;
         this.cardSuit = cardSuit;
