593988da9d79b6a377ac2d4c5c492c77e3ac79bc
paerma <555rostik556@ukr.net>
2026-05-20 18:15:21 +0200

    fix: deck list encapsulation and remove from list optimization

--- src/main/java/com/example/blackjack/Deck.java
+++ src/main/java/com/example/blackjack/Deck.java
@@ -6,28 +6,38 @@ import java.util.List;
 
 
 public class Deck {
-    List<Card> deck = new ArrayList<>();
+    private List<Card> deck = new ArrayList<>();
 
     public Deck(){
         createDeck();
-        randomize();
+        shuffle();
     }
 
 
-    public void randomize(){
+    public void shuffle(){
         Collections.shuffle(deck);
     }
 
     public void createDeck() {
         for (Suits card : Suits.values()) {
-            for (int i = 0; i < 11; i++) {
-                deck.add(new Card(i + 1, card));
+            for (int i = 2; i <= 14; i++) {
+                if(i >= 10 && i < 14){
+                    deck.add(new Card(10, card));
+                }
+                else if(i == 14){
+                    deck.add(new Card(11, card));
+                }
+                else deck.add(new Card(i, card));
             }
         }
     }
 
+    public int deckSize(){
+        return deck.size();
+    }
+
     public Card giveCard(){
-        return deck.remove(0);
+        return deck.remove(deck.size()-1);
     }
     public void newDeck() {
         deck.clear();
