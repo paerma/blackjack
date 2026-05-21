b1fc6cfd803172cd327b804e49fe3ade7e8a0f07
paerma <555rostik556@ukr.net>
2026-05-21 19:37:35 +0200

    feat: added colors and symbols

--- src/main/java/com/example/blackjack/Suits.java
+++ src/main/java/com/example/blackjack/Suits.java
@@ -1,8 +1,22 @@
 package com.example.blackjack;
 
 public enum Suits {
-    HEARTS,
-    DIAMONDS,
-    CLUBS,
-    SPADES,
+    HEARTS("♥", "red"),
+    DIAMONDS("♦", "red"),
+    CLUBS("♣", "black"),
+    SPADES("♠", "black");
+
+    private final String symbol;
+    private final String color;
+
+    Suits(String symbol, String color){
+        this.color = color;
+        this.symbol = symbol;
+    }
+    public String getSymbol(){
+        return symbol;
+    }
+    public String getColor(){
+        return color;
+    }
 }
